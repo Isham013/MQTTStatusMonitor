d68a52a8c99f177ea52f6aedcd60aa5fcb002ddd
Isham013 <96410403+Isham013@users.noreply.github.com>
2024-07-05 01:25:23 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,3 @@
-MONGO_URL = "mongodb+srv://isham:WkvsN1Qv1CXIkxLJ@clustermqtt.jjdzcma.mongodb.net/?retryWrites=true&w=majority&appName=ClusterMqtt"
 MONGO_DATABASE = "Mqtt"  # Database name
 MONGO_COLLECTION = "messages"  # Collection name
 broker_address = "localhost"
